fd3c1372bf8c7f40111e507da0cffe4bee7e1ad2
tamu-dev <tanmaymp92@gmail.com>
2026-01-30 18:42:29 +0530

    factorial

---
+++ StriverJava-master/src/Basic/Recursion/A4_FactorialOfNumber.java
@@ -0,0 +1,22 @@
+package Basic.Recursion;
+
+import java.util.Scanner;
+
+public class A4_FactorialOfNumber {
+    public static int factorial(int n) {
+        if(n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        int result = factorial(num);
+        System.out.println(result);
+    }
+}
