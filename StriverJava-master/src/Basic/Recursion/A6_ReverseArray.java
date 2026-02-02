630d75c4e4ba58fd5f6dd397d8e2565b0dcf852d
tamu-dev <tanmaymp92@gmail.com>
2026-02-02 20:48:28 +0530

    basic recursion

--- StriverJava-master/src/Basic/Recursion/A6_ReverseArray.java
+++ StriverJava-master/src/Basic/Recursion/A6_ReverseArray.java
@@ -1,19 +1,30 @@
 package Basic.Recursion;
+import java.util.Scanner;
+
+import static java.util.Collections.swap;
 
 public class A6_ReverseArray {
-    public void reverse(int[] arr, int n,int start,int end) {
+    public static void reverse(int[] arr, int n,int i) {
+        if(i>=n/2) return;
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+        reverse(arr,n,i+1);
+    }
 
-        if (start >= end) {
-            return;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Array size");
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
         }
-
-        // swap
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        // recursive call
-        reverse(arr, start + 1, end - 1);
+        reverse(arr,arr.length,0);
+        for(int num: arr){
+            System.out.print(num+" ");
+        }
     }
 }
 
