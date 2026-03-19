270d87ccb64f71b6c9c4b8352bc28b24d6b74ca4
tamu-dev <tanmaymp92@gmail.com>
2026-03-19 23:30:01 +0530

    class Node for the LL

--- StriverJava-master/src/Stack_And_Queue/Learning/Easy/Node.java
+++ StriverJava-master/src/Stack_And_Queue/Learning/Easy/Node.java
@@ -3,8 +3,11 @@ package Stack_And_Queue.Learning.Easy;
 class Node{
     int val;
     Node next;
-    Node(int d){
-        val = d;
+    Node(){
+        this.next = null;
+    }
+    Node(int val){
+        this.val = val;
         next = null;
     }
 }
