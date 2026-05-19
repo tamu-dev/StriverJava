75eeb46d12bc9fd69e6dd05589f906c67ed1a63f
tamu-dev <tanmaymp92@gmail.com>
2026-05-19 10:56:37 +0530

    Binary search medium

---
+++ StriverJava-master/src/BinarySearch/BS_on_1D_Arrays/Medium/A08_AggressiveCows.java
@@ -0,0 +1,51 @@
+package BinarySearch.BS_on_1D_Arrays.Medium;
+
+import java.util.Scanner;
+
+public class A08_AggressiveCows {
+    private static boolean canWePlace(int [] stalls,int cow,int d){
+        int count = 1;
+        int lastPos = stalls[0];
+
+        for(int i=1; i<stalls.length;i++){
+            if(stalls[i] - lastPos >= d){
+                count++;
+                lastPos = stalls[i];
+            }
+            if(count >= cow) return true;
+        }
+        return false;
+    }
+    public static int aggressiveCows(int[] stalls, int cow) {
+        int low = 1;
+        int high = stalls[stalls.length-1]- stalls[0];
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(canWePlace(stalls,cow,mid)){
+                ans = mid;
+                low = mid +1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return  ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Array size: ");
+        int n = sc.nextInt();
+
+        int [] arr = new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+
+        System.out.println("Enter number of cows");
+        int k = sc.nextInt();
+
+        System.out.println("maximum possible minimum distance "+aggressiveCows(arr,k));
+    }
+}
