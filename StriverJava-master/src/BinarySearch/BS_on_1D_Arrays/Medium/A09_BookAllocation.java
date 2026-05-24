d32fd5e68f503efbcf941fcd0cba4fb13e90c790
tamu-dev <tanmaymp92@gmail.com>
2026-05-24 21:47:17 +0530

    Binary Search Medium

--- StriverJava-master/src/BinarySearch/BS_on_1D_Arrays/Medium/A09_BookAllocation.java
+++ StriverJava-master/src/BinarySearch/BS_on_1D_Arrays/Medium/A09_BookAllocation.java
@@ -1,4 +1,52 @@
 package BinarySearch.BS_on_1D_Arrays.Medium;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class A09_BookAllocation {
+    private static int student(int [] arr,int pages){
+        int student = 1;
+        int pagesStudent = 0;
+        for(int i=0;i<arr.length;i++){
+            if(pagesStudent + arr[i] <= pages){
+                pagesStudent += arr[i];
+            }else{
+                student++;
+                pagesStudent += arr[i];
+            }
+        }
+        return student;
+    }
+    public static int findPages(int[] nums, int m) {
+        int low = Arrays.stream(nums).min().getAsInt();
+        int high = Arrays.stream(nums).sum();
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int student = student(nums,mid);
+            if(student > m){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Array size: ");
+        int n = sc.nextInt();
+
+        int [] arr = new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+
+        System.out.println("Enter number of students");
+        int k = sc.nextInt();
+
+        System.out.print("The answer is: "+findPages(arr,k));
+    }
 }
--- StriverJava-master/src/BinarySearch/BS_on_1D_Arrays/Medium/A10_PaintersPartition.java
+++ StriverJava-master/src/BinarySearch/BS_on_1D_Arrays/Medium/A10_PaintersPartition.java
@@ -1,4 +1,54 @@
 package BinarySearch.BS_on_1D_Arrays.Medium;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class A10_PaintersPartition {
+    private static int countPainters(int [] arr,int k){
+        int painters = 1;
+        int painterCount = 0;
+        for(int i=0;i<arr.length;i++){
+            if (painterCount + arr[i] <= k){
+                painterCount += arr[i];
+            }
+            else{
+                painters++;
+                painterCount = arr[i];
+            }
+        }
+        return painters;
+    }
+    public static int findLargestMinimumSum(int[] nums, int m) {
+        int low = Arrays.stream(nums).max().getAsInt();
+        int high = Arrays.stream(nums).sum();
+        int result = high;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int student = countPainters(nums,mid);
+            if(student > m){
+                low = mid +1;
+            }
+            else{
+                result = mid;
+                high = mid -1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Array size: ");
+        int n = sc.nextInt();
+
+        int [] arr = new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+
+        System.out.println("Enter number of students");
+        int k = sc.nextInt();
+
+        System.out.print("The answer is: "+findLargestMinimumSum(arr,k));
+    }
 }
